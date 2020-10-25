4ef42452b47bc2e6422568ad8f05386aa3e723f0
royalturd <samridhh.rudra@hotmail.com>
2020-10-26 03:29:15 +0530

    File handling: open,write,close
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

---
+++ mymodules/demo.py
@@ -0,0 +1,3 @@
+f = open("demo.txt", "a")                                       # creates a file name demo.txt
+f.write("Go and get a life......... (cries in corner)  ")       # writes to that file
+f.close()                                                       # close the file
