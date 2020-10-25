56c9efcadd83d29ad683d4508e5d3b3491f05d0a
royalturd <samridhh.rudra@hotmail.com>
2020-10-26 03:35:26 +0530

    File handling: open file in python shell
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

--- mymodules/demo.py
+++ mymodules/demo.py
@@ -1,3 +1,8 @@
 f = open("demo.txt", "a")                                       # creates a file name demo.txt
 f.write("Go and get a life......... (cries in corner)  ")       # writes to that file
 f.close()                                                       # close the file
+
+## to read the file in terminal ##
+
+f = open("demo.txt", "r")                                       # r to make the file readable 
+print(f.read())
