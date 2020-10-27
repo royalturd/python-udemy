1c82f57e39147ca80adf798e98aa606e83b72203
royalturd <samridhh.rudra@hotmail.com>
2020-10-28 03:44:05 +0530

    Added: create object

---
+++ random code/classes/init.py
@@ -0,0 +1,9 @@
+# build class named person and use init
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+obj1 = person("Rudra", 20)
+    
+print(obj1.name)
+print(obj1.age)
---
+++ random code/classes/object.py
@@ -0,0 +1,6 @@
+# create first object
+class firstclass:
+    x = 69
+
+obj1 = firstclass()
+print(obj1.x)
