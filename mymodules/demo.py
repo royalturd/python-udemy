38c2639ae9fed4d9bb4826b3ff16cf3cd0327e00
pratyakshm <pratyakshm@protonmail.com>
2020-10-30 23:58:11 +0530

    repo: protyakshm changes

--- mymodules/demo.py
+++ mymodules/demo.py
@@ -1,5 +1,6 @@
 f = open("demo.txt", "a")                                       # creates a file name demo.txt
-f.write("Go and get a life......... (cries in corner)  ")       # writes to that file
+f.write("Go and get a life...")
+f.write(" *cries in corner*  ")      				# writes to that file
 f.close()                                                       # close the file
 
 ## to read the file in terminal ##
