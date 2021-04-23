d6f87cba4232583eba82d8833c01ac61a1cc45e6
royalturd <samridhh.rudra@hotmail.com>
2021-04-24 00:28:58 +0530

    Added: Hangman
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

---
+++ mymodules/Hangman/hangman.py
@@ -0,0 +1,98 @@
+import random
+def hangman():
+
+    word = random.choice(["pugger" , "superman" , "pokemon" , "royalturd" , "loki" , "thor" , "tiger" , "nobody" , "falcon" , "thanos"])
+    validletters = 'abcdefghijklmnopqrstuvwxyz'
+    turns = 10
+    guessmade = ''
+    while len(word) > 0:
+        main = ""
+        missed = 0
+
+        for letter in word:
+            if letter in guessmade:
+                main = main + letter
+            else:
+                main = main + "_" + " "
+
+        if main == word:
+            print(main)
+            print("You won!")
+            break
+        print("guess the word: " + main)
+        guess = input()
+
+        if guess in validletters:
+            guessmade = guessmade + guess
+        else:
+            print("Enter a valid letter")
+            guess = input()
+
+        if guess not in word:
+            turns = turns - 1
+            if turns == 9:
+                print("9 turns left")
+                print("  --------  ")
+            if turns == 8:
+                print("8 turns left")
+                print("  --------  ")
+                print("     O      ")
+            if turns == 7:
+                print("7 turns left")
+                print("  --------  ")
+                print("     O      ")
+                print("     |      ")
+            if turns == 6:
+                print("6 turns left")
+                print("  --------  ")
+                print("     O      ")
+                print("     |      ")
+                print("    /       ")
+            if turns == 5:
+                print("5 turns left")
+                print("  --------  ")
+                print("     O      ")
+                print("     |      ")
+                print("    / \     ")
+            if turns == 4:
+                print("4 turns left")
+                print("  --------  ")
+                print("   \ O      ")
+                print("     |      ")
+                print("    / \     ")
+            if turns == 3:
+                print("3 turns left")
+                print("  --------  ")
+                print("   \ O /    ")
+                print("     |      ")
+                print("    / \     ")
+            if turns == 2:
+                print("2 turns left")
+                print("  --------  ")
+                print("   \ O /|   ")
+                print("     |      ")
+                print("    / \     ")
+            if turns == 1:
+                print("1 turns left")
+                print("Last breaths counting, Take care!")
+                print("  --------  ")
+                print("   \ O_|/   ")
+                print("     |      ")
+                print("    / \     ")
+            if turns == 0:
+                print("You loose")
+                print("You let a kind man die")
+                print("  --------  ")
+                print("     O_|    ")
+                print("    /|\      ")
+                print("    / \     ")
+                break
+
+
+
+name = input("Enter your name: ")
+print("welcome" , name )
+print("--------------------------")
+print(" now try to gess the word only 10 tries left ")
+hangman()
+print()
