fc11eb7e0a33360173617bf0b70be7d157a8e0be
royalturd <samridhh.rudra@hotmail.com>
2021-05-01 03:23:56 +0530

    TicTacToe: finised
    Signed-off-by: royalturd <samridhh.rudra@hotmail.com>

--- mymodules/TicTacToe/tictactoe.py
+++ mymodules/TicTacToe/tictactoe.py
@@ -3,10 +3,10 @@ board = [' ' for x in range(10)]
 def insertLetter(letter,pos):
     board[pos] = letter
 
-def spaceisfree(pos):
+def spaceIsFree(pos):
     return board[pos] == ' '
 
-def boardlayout(board):
+def printBoard(board):
     print('   |   |   ')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |   ')
@@ -19,13 +19,13 @@ def boardlayout(board):
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |   ')
 
-def isboardfull(board):
+def isBoardFull(board):
     if board.count(' ') > 1:
         return False 
     else:
         return True
 
-def iswinner(b,l):
+def IsWinner(b,l):
     return ((b[1] == l and b[2] == l and b[3] == l) or
     (b[4] == l and b[5] == l and b[6] == l) or
     (b[7] == l and b[8] == l and b[9] == l) or
@@ -35,14 +35,14 @@ def iswinner(b,l):
     (b[1] == l and b[5] == l and b[9] == l) or             
     (b[3] == l and b[5] == l and b[7] == l))
 
-def playermove():
+def playerMove():
     run = True
     while run:
         move = input("Please select a position to enter the X b\w 1-9: ")
         try:
             move = int(move)
             if move > 0 and move < 10:
-                if spaceisfree(move):
+                if spaceIsFree(move):
                  run = False
                  insertLetter('X' , move)
                 else:
@@ -53,15 +53,80 @@ def playermove():
             print("Please type a number :")
 
 def computerMove(): #ai
-    possiblemoves = [x for x , letter in enumerate(board) if letter == '' and X != 0]
+    possibleMoves = [x for x , letter in enumerate(board) if letter == ' ' and x != 0  ]
     move = 0
 
-    for let in ['0' , 'X']:
-        for i in possiblemoves:
+    for let in ['O' , 'X']:
+        for i in possibleMoves:
             boardcopy = board[:]
             boardcopy[i] = let
-            if iswinner(boardcopy, let):
+            if IsWinner(boardcopy, let):
                 move = i
                 return move
+    
+    cornersOpen = [] 
+    for i in possibleMoves:
+        if i in [1 , 3 , 7 , 9]:
+            cornersOpen.append(i)
 
-        
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            edgesOpen.append(i)
+
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+        return move
+
+def selectRandom(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0,ln)
+    return li[r]
+
+def main():
+    print("Welcome to the game!")
+    printBoard(board)
+
+    while not(isBoardFull(board)):
+        if not(IsWinner(board , 'O')):
+            playerMove()
+            printBoard(board)
+        else:
+            print("You loose! buddy")
+            break
+
+        if not(IsWinner(board , 'X')):
+            move = computerMove()
+            if move == 0:
+                print(" ")
+            else:
+                insertLetter('O' , move)
+                print('computer placed an o on position' , move , ':')
+                printBoard(board)
+        else:
+            print("you win buddy")
+            break
+
+
+
+
+    if isBoardFull(board):
+        print("Tie game")
+
+while True:
+    x = input("Do you want to play again? (y/n) : ")
+    if x.lower() == 'y':
+        board = [' ' for x in range(10)]
+        print('--------------------')
+        main()
+    else:
+        break
